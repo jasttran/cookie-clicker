59223d68c4cdda32563359fc542a72d72c507007
Jasmine <jastran1003@gmail.com>
2022-12-09 11:34:24 +1100

    missing files added

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import auth from './routes/auth.js'
 const app = express();
 
-app.listen(5001, () => console.log('API is running on http://localhost:3000/login'));
+
+app.use(cors()); // cross origin resource sharing
+app.use(express.json()); // built-in middleware for json
+
+// route any request coming for the /login to the router, instead of the 
+// other routes below. require the router we have created
+app.use('/auth', auth);
+
+app.listen(5002, () => console.log('API is running on http://localhost:3000/login'));
